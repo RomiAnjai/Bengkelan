7db5dfb87d3321603b4cef0b6bf053e78d1d78a8
RomiAnjai <mirzajoss6969@gmail.com>
2026-08-31 16:38:11 +0700

    Slakop
    Update :
    - Import Asset motor ke unity

--- Assets/Asset Programmer Romi/Script/GameManagerScript.cs
+++ Assets/Asset Programmer Romi/Script/GameManagerScript.cs
@@ -12,6 +12,9 @@ public class GameManagerScript : MonoBehaviour
     public static GameManagerScript instance;
     public GameState currentState;
     public GameObject SpawnObjek;
+    public Transform posisiSpawn;
+
+    private Rigidbody rbBan;
 
     void Awake()
     {
@@ -35,16 +38,34 @@ public class GameManagerScript : MonoBehaviour
         switch (currentState)
         {
             case GameState.MotorBelumDatang:
-                Vector3 position = new Vector3(0f, 0f, 0f);
-                Instantiate(SpawnObjek, position, Quaternion.identity);
-                currentState = GameState.MotorSedangServis;
+                Vector3 targetPosisiSpawn = posisiSpawn.position;
+                if (SpawnObjek != null)
+                {
+                    Instantiate(SpawnObjek, targetPosisiSpawn, Quaternion.identity);
+                    AmbilDataBan();
+                    currentState = GameState.MotorSedangServis;
+                }
                 break;
 
             case GameState.MotorSedangServis:
+                if (rbBan != null && Input.GetKeyDown(KeyCode.E))
+                {
+                    rbBan.isKinematic = false;
+                    rbBan.useGravity = true;
+                }
                 break;
 
             case GameState.MotorSelesai:
                 break;
+        }
+    }
+
+    void AmbilDataBan()
+    {
+        GameObject ban = GameObject.FindWithTag("Ban");
+        if (ban != null)
+        {
+            rbBan = ban.GetComponent<Rigidbody>();
         }
     }
 }
