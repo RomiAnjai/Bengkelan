3fb33e9bff9eed7c6b18b3a23e58c2ddeb523bc4
RomiAnjai <mirzajoss6969@gmail.com>
2026-09-02 12:10:08 +0700

    baut muter

--- Assets/Asset Programmer Romi/Script/Baut.cs
+++ Assets/Asset Programmer Romi/Script/Baut.cs
@@ -4,29 +4,47 @@ public class Baut : MonoBehaviour
 {
     public string namaObjek = "Baut";
     public DataSparepart partInduk;
+    public SphereCollider colliderBan;
 
     [Header("Kustomisasi Gerakan Baut")]
     public float kecepatanPutar = 500f;
     public float jarakKeluar = 0.2f;
     public Vector3 arahKeluar = new Vector3(0, 0, 1);
+    public Vector3 arahputar;
 
     [Header("Rotasi Awal & Dikunci")]
     public float rotasiKunciY = 90f;
     public float rotasiKunciZ = 90f;
+    public char arahPutar;
 
     public float progres = 0f;
     private Vector3 posisiAwal;
     private float rotasiXSaatIni;
+    private float rotasiYSaatIni;
+    private float rotasiZSaatIni;
 
     void Start()
     {
         posisiAwal = transform.localPosition;
 
-        rotasiXSaatIni = transform.localEulerAngles.x;
+        if (arahPutar == 'x')
+        {
+            rotasiXSaatIni = transform.localEulerAngles.x;
+        } else if (arahPutar == 'y')
+        {
+            rotasiXSaatIni = transform.localEulerAngles.y;
+        } else if (arahPutar == 'z')
+        {
+            rotasiXSaatIni = transform.localEulerAngles.z;
+        }
 
         if (partInduk != null)
         {
             partInduk.bisaDiambil = false;
+            if (colliderBan != null)
+            {
+                colliderBan.enabled = false;   
+            }
         }
     }
 
@@ -34,9 +52,13 @@ public class Baut : MonoBehaviour
     {
         progres += Time.deltaTime;
 
-        rotasiXSaatIni -= kecepatanPutar * Time.deltaTime;
+        //rotasiXSaatIni -= kecepatanPutar * Time.deltaTime;
+        //transform.localEulerAngles = new Vector3(rotasiXSaatIni, rotasiKunciY, rotasiKunciZ);
 
-        transform.localEulerAngles = new Vector3(rotasiXSaatIni, rotasiKunciY, rotasiKunciZ);
+        //Vector3 arahkunci; 
+
+        Vector3 deltaputar = arahputar * kecepatanPutar * Time.deltaTime;
+        transform.localEulerAngles = transform.localEulerAngles + deltaputar;
 
         Vector3 deltaPosisi = arahKeluar * (progres * jarakKeluar);
         transform.localPosition = posisiAwal + deltaPosisi;
@@ -53,6 +75,10 @@ public class Baut : MonoBehaviour
         if (sisaBaut.Length <= 1)
         {
             partInduk.bisaDiambil = true;
+            if (colliderBan != null)
+            {
+                colliderBan.enabled = true;   
+            }
         }
         Destroy(gameObject);
     }
